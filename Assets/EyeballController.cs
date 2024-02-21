2e77f1e3cea1e158f3758296ffc475bccc376c7a
Simon-NTI <simon.hall@elev.ga.nti.gse>
2024-02-21 15:04:15 +0100

    Initial commit

---
+++ Assets/EyeballController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EyeBallController : MonoBehaviour
+{
+    GameObject headCamera;
+    [SerializeField] Vector2 sensitivity = new(5, 5);
+    [SerializeField] float pitchLimit = 80;
+    float cameraRotationX = 0;
+
+
+    void Awake()
+    {
+        headCamera = GetComponentInChildren<Camera>().gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    void OnLook(InputValue value)
+    {
+        Vector2 lookVector = value.Get<Vector2>();
+
+        float degreesY = lookVector.x * sensitivity.x;
+        float degreesX = lookVector.y * sensitivity.y;
+        transform.Rotate(Vector3.up, degreesY);
+
+        cameraRotationX += degreesX;
+        cameraRotationX = Math.Clamp(cameraRotationX, -pitchLimit, pitchLimit);
+        headCamera.transform.localEulerAngles = new(-cameraRotationX, 0, 0);
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/MovementController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementController : MonoBehaviour
+{
+    Vector2 inputVector = Vector2.zero;
+    [SerializeField] float speed = 5;
+    [SerializeField] float jumpForce = 5;
+    [SerializeField] float gravityMultiplier = 5;
+    CharacterController characterController;
+    float velocityY = 0;
+    bool jumpPressed = false;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+
+        Vector3 movement = transform.right * inputVector.x
+        + transform.forward * inputVector.y;
+        movement *= speed;
+
+        if (characterController.isGrounded)
+        {
+            velocityY = -1f;
+            if(jumpPressed)
+            {
+                velocityY = jumpForce;
+            }
+        }
+
+        velocityY += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
+        movement.y = velocityY;
+
+        characterController.Move(movement * Time.deltaTime);
+        jumpPressed = false;
+    }
+
+    void OnMove(InputValue value) => inputVector = value.Get<Vector2>();
+        
+    void OnJump(InputValue value) => jumpPressed = true;
+}
